f9744b1623d7c5f5d236092b23f033fd8ca2ba51
nicolapitzalis <47148456+nicolapitzalis@users.noreply.github.com>
2023-11-22 17:10:15 +0100

    base nn architecture

--- layer.py
+++ layer.py
@@ -3,63 +3,101 @@ import numpy as np
 
 class Layer():
     """
-    Represents a layer in a neural network.
+    A class representing a neural network layer.
+
+    Args:
+        input_size (int): The size of the input to the layer.
+        output_size (int): The size of the output from the layer.
+        activation (Callable[[np.ndarray], np.float32], optional): The activation function to be applied to the layer's output. Defaults to None.
+        activation_prime (Callable[[np.ndarray], np.float32], optional): The derivative of the activation function. Defaults to None.
 
     Attributes:
-    - inputs: The input values to the layer.
-    - weights: The weights associated with the inputs.
-    - activation: The activation function applied to the net input.
-    - outputs: The output values of the layer.
-    - errors: The error values of the layer.
-
-    Methods:
-    - __init__: Initializes the Layer object.
-    - net: Calculates the net input of the layer.
-    - forward: Performs the forward pass of the layer.
+        input_size (int): The size of the input to the layer.
+        output_size (int): The size of the output from the layer.
+        activation (Callable[[np.ndarray], np.float32]): The activation function to be applied to the layer's output.
+        activation_prime (Callable[[np.ndarray], np.float32]): The derivative of the activation function.
+        weight (np.ndarray): The weight of the layer.
+        input (np.ndarray): The input to the layer.
+        bias (np.ndarray): The bias of the layer.
+        net (np.ndarray): The net input to the layer.
+        output (np.ndarray): The output of the layer.
+        delta (np.ndarray): The error of the layer.
+        delta_weight (np.ndarray): The weight updates of the layer.
+        delta_bias (np.ndarray): The bias updates of the layer.
     """
-    
-    def __init__(self, inputs: np.ndarray, weights: np.ndarray, activation: Callable[[np.ndarray], np.float32]):
-        self.inputs = inputs
+
+    def __init__(self, 
+                 input_size: int,
+                 output_size: int,
+                 activation: Callable[[np.ndarray], np.float32] = None, 
+                 activation_prime: Callable[[np.ndarray], np.float32] = None):
+        self.input_size = input_size
+        self.output_size = output_size
         self.activation = activation
-        self.weights = weights
-        self.net: np.ndarray
-        self.outputs: np.float32
-        self.delta: np.ndarray
-        self.delta_weights: np.ndarray
-        self.activation_prime: Callable[[Callable[[np.ndarray], np.float32]], Callable[[np.ndarray], np.float32]]
+        self.activation_prime = activation_prime
+        self.weight = self.weight_init()
+        self.input = np.ndarray = None
+        self.bias: np.ndarray = None
+        self.net: np.ndarray = None
+        self.output: np.ndarray = None
+        self.delta: np.ndarray = np.zeros(self.output_size)
+        self.delta_weight: np.ndarray = np.zeros((self.output_size, self.input_size))
+        self.delta_bias: np.ndarray = np.zeros(self.output_size)
+
+
+    def set_input(self, input_data: np.ndarray):
+        """
+        Sets the input data for the layer.
+        """
+        self.input = input_data
+        self.output = input_data
 
     def compute_net(self) -> np.ndarray:
         """
-        Calculates the net input of the layer.
+        Computes the net input to the layer.
 
         Returns:
-        - The net input value.
+            np.ndarray: The net input to the layer.
         """
-        return np.matmul(self.weights, self.inputs)
+        return np.matmul(self.weight, self.input)
 
     def forward(self) -> np.ndarray:
         """
         Performs the forward pass of the layer.
 
         Returns:
-        - The output values of the layer.
+            np.ndarray: The output of the layer.
         """
         self.net = self.compute_net()
-        self.outputs = self.activation(self.net)
-        return self.outputs
+        self.output = self.activation(self.net)
+        return self.output
     
     def backward(self, delta_k: np.ndarray) -> np.ndarray:
         """
         Performs the backward pass of the layer.
 
         Args:
-            delta_k (np.ndarray): The error gradient of the following layer.
+            delta_k (np.ndarray): The error of the next layer.
 
         Returns:
-            np.ndarray: The error gradient of the layer.
+            np.ndarray: The error of the current layer.
         """
         act_prime = self.activation_prime(self.net)
         np.multiply(delta_k, act_prime, out=self.delta)
-        self.delta = np.matmul(self.weights.T, self.delta)          # computing the error on the present layer
-        self.delta_weights = np.outer(self.delta, self.inputs)     # updating the weights
+        self.delta = np.matmul(self.weight.T, self.delta)          # computing the error on the present layer
+        self.delta_weight = np.outer(self.delta, self.input)      # updating the weight
         return self.delta
+    
+    def weight_init(self) -> np.ndarray:
+        """
+        Initializes the weight of the layer.
+
+        Returns:
+            np.ndarray: The initialized weight.
+        """
+
+        # TODO: implement different weight initialization schemes
+
+        weight = np.random.randn(self.output_size, self.input_size)
+        return weight
+
---
+++ neural_network.py
@@ -0,0 +1,112 @@
+from typing import Callable, List
+import numpy as np
+from layer import Layer
+
+class NeuralNetwork():
+    """
+    A class representing a neural network.
+
+    Parameters:
+    - hidden_layer_sizes (List[int]): List of integers representing the number of units in each hidden layer.
+    - n_hidden_layers (int): Number of hidden layers in the network.
+    - n_output_units (int): Number of output units in the network.
+    - task (str): The type of task the network is designed for (e.g., classification, regression).
+    - loss (Callable[[np.ndarray, np.ndarray], np.float32]): Loss function used to compute the network's error.
+    - loss_prime (Callable[[np.ndarray, np.ndarray], np.float32]): Derivative of the loss function.
+    - activation_hidden (Callable[[np.ndarray], np.float32]): Activation function used in the hidden layers.
+    - activation_hidden_prime (Callable[[np.ndarray], np.float32]): Derivative of the activation function used in the hidden layers.
+    - activation_output (Callable[[np.ndarray], np.float32]): Activation function used in the output layer.
+    - activation_output_prime (Callable[[np.ndarray], np.float32]): Derivative of the activation function used in the output layer.
+    - learning_rate (float): Learning rate used in the network's training algorithm.
+    """
+
+    def __init__(self, 
+                 hidden_layer_sizes: List[int], 
+                 n_hidden_layers: int, 
+                 n_output_units: int,
+                 task: str,
+                 loss: Callable[[np.ndarray, np.ndarray], np.float32], 
+                 loss_prime: Callable[[np.ndarray, np.ndarray], np.float32],
+                 activation_hidden: Callable[[np.ndarray], np.float32],
+                 activation_hidden_prime: Callable[[np.ndarray], np.float32],
+                 activation_output: Callable[[np.ndarray], np.float32],
+                 activation_output_prime: Callable[[np.ndarray], np.float32],
+                 learning_rate: float):
+        """
+        Initialize the NeuralNetwork object.
+
+        Args:
+        - hidden_layer_sizes (List[int]): List of integers representing the number of units in each hidden layer.
+        - n_hidden_layers (int): Number of hidden layers in the network.
+        - n_output_units (int): Number of output units in the network.
+        - task (str): The type of task the network is designed for (e.g., classification, regression).
+        - loss (Callable[[np.ndarray, np.ndarray], np.float32]): Loss function used to compute the network's error.
+        - loss_prime (Callable[[np.ndarray, np.ndarray], np.float32]): Derivative of the loss function.
+        - activation_hidden (Callable[[np.ndarray], np.float32]): Activation function used in the hidden layers.
+        - activation_hidden_prime (Callable[[np.ndarray], np.float32]): Derivative of the activation function used in the hidden layers.
+        - activation_output (Callable[[np.ndarray], np.float32]): Activation function used in the output layer.
+        - activation_output_prime (Callable[[np.ndarray], np.float32]): Derivative of the activation function used in the output layer.
+        - learning_rate (float): Learning rate used in the network's training algorithm.
+        """
+        
+        self.layers: List[Layer] = []
+        self.n_features: int = None
+        self.hidden_layer_sizes = hidden_layer_sizes
+        self.n_hidden_layers = n_hidden_layers
+        self.n_output_units = n_output_units
+        self.task = task
+        self.loss = loss
+        self.loss_prime = loss_prime
+        self.activation_hidden = activation_hidden
+        self.activation_hidden_prime = activation_hidden_prime
+        self.activation_output = activation_output
+        self.activation_output_prime = activation_output_prime
+        self.learning_rate = learning_rate
+
+    def _add_layer(self, input_size, output_size, activation=None, activation_prime=None):
+        """
+        Add a layer to the neural network.
+
+        Args:
+        - input_size (int): Number of input units for the layer.
+        - output_size (int): Number of output units for the layer.
+        - activation (Callable[[np.ndarray], np.float32]): Activation function used in the layer.
+        - activation_prime (Callable[[np.ndarray], np.float32]): Derivative of the activation function used in the layer.
+        """
+        self.layers.append(Layer(input_size, output_size, activation, activation_prime))
+
+    def _network_architecture(self):
+        """
+        Define the architecture of the neural network by adding layers.
+        """
+        # Add input layer
+        self._add_layer(self.n_features, self.n_features)
+        
+        # Add hidden layers
+        for i in range(self.n_hidden_layers):
+            if i == 0:
+                self._add_layer(self.hidden_layer_sizes[i], self.n_features, self.activation_hidden, self.activation_hidden_prime)
+            else:
+                self._add_layer(self.hidden_layer_sizes[i], self.hidden_layer_sizes[i-1], self.activation_hidden, self.activation_hidden_prime)
+        
+        # Add output layer
+        self._add_layer(self.hidden_layer_sizes[-1], self.n_output_units, self.activation_output, self.activation_output_prime)
+        
+    def _predict_ouptuts(self, features):
+        """
+        Predict the outputs of the neural network for the given features.
+
+        Args:
+        - features (np.ndarray): Input features for prediction.
+
+        Returns:
+        - output (np.ndarray): Predicted outputs of the neural network.
+        """
+        self.layers[0].pass_input(features)
+
+        for i in range(1, len(self.layers)):
+            self.layers[i].input = self.layers[i-1].output
+            output = self.layers[i].forward()
+        
+        return output
+
