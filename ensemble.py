ebe7d47ee3f6e90ad2e440cd68a9f3558346b15f
nicolapitzalis <47148456+nicolapitzalis@users.noreply.github.com>
2024-01-09 10:17:08 +0100

    fix ensemble

--- ensemble.py
+++ ensemble.py
@@ -1,5 +1,3 @@
-from math_functions.function_enums import Metrics
-from math_functions.loss import mse, mee
 import numpy as np
 
 class Ensemble:
@@ -15,11 +13,3 @@ class Ensemble:
     def predict(self, data: np.array) -> np.array:
         self.predictions = [model.predict(data) for model in self.models]
         return np.mean(self.predictions, axis=0)
-    
-    def evaluate(self, y_pred: np.array, y_true: np.array, metric: Metrics) -> float:
-        if metric == Metrics.MSE.value:
-            return mse(y_true, y_pred)
-        elif metric == Metrics.MEE.value:
-            return mee(y_true, y_pred)
-        else:
-            raise Exception(f"Metric {metric} not supported.")
